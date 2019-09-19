e60e447ead0924d8bc65e919182b60ae9a09f7a5
Andrei Pan <pan.andreico@gmail.com>
2019-09-19 12:41:54 +0300

    Update plugins, remove unnecessary plugins

--- js/main.js
+++ js/main.js
@@ -1,64 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.pointer, #map a').popover();
 
-	$('nav#menu').mmenu();
-
-        $('#testimony').rhinoslider({
-		effect: 'fade',
-		controlsKeyboard: false,
-		controlsPrevNext: false,
-		controlsPlayPause: false,
-		autoPlay: true,
-		pauseOnHover: false,
-		showBullets: 'never',
-		showControls: 'never',
-                showTime : 5000
-	});
-
-	$('.slide').rhinoslider({
-		effect: 'fade',
-		controlsKeyboard: false,
-		controlsPrevNext: false,
-		controlsPlayPause: false,
-		autoPlay: true,
-		pauseOnHover: false,
-		showBullets: 'never',
-		showControls: 'never',
-                showTime : 5000
-    });
-    
-
     $('.slider-items').slick({
-        prevArrow : "#slider .arrow-prev",
-        nextArrow : "#slider .arrow-next"
+        prevArrow: "#slider .arrow-prev",
+        nextArrow: "#slider .arrow-next"
     });
 
-    if($('.fancy').size() > 0) {
-    	$('.fancy').fancybox();
-    	$('a.fancy').on("click", function(e){
-    		e.preventDefault();
-    	});
-    }
-
-    if($("#sequence").size() > 0)
-    {
-        var options = {
-            nextButton: false,
-            prevButton: false,
-            pagination: true,
-            animateStartingFrameIn: true,
-            autoPlay: true,
-            autoPlayDelay: 5000,
-            preloader: true,
-            preloadTheseFrames: [1],
-            preloadTheseImages: [
-                "img/slider/tn-model1.png",
-                "img/slider/tn-model2.png",
-                "img/slider/tn-model3.png"
-            ]
-        };
-        var mySequence = $("#sequence").sequence(options).data("sequence");
+    if ($('.fancy').length > 0) {
+        $('.fancy').fancybox();
+        $('a.fancy').on("click", function (e) {
+            e.preventDefault();
+        });
     }
 
     $('input[name=jscode]').val('1');
@@ -75,9 +28,15 @@ function bannerLoadImg() {
     $('.lzy').each(function (index) {
         var el = $(this);
 
-        if (sWidth > 0) { dataProp = 'mobile'; }
-        if (sWidth > 360 && el.data('tablet') != undefined) { dataProp = 'tablet'; }
-        if (sWidth > 767 && el.data('desktop') != undefined) { dataProp = 'desktop'; }
+        if (sWidth > 0) {
+            dataProp = 'mobile';
+        }
+        if (sWidth > 360 && el.data('tablet') != undefined) {
+            dataProp = 'tablet';
+        }
+        if (sWidth > 767 && el.data('desktop') != undefined) {
+            dataProp = 'desktop';
+        }
         if (el.attr('src') != el.data(dataProp) && el.data(dataProp)) {
             el.attr('src', el.data(dataProp));
         }
