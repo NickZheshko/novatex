9e650dde41f9d870d61718c4360c30c11f6c09c6
Andrei Pan <pan.andreico@gmail.com>
2019-11-22 14:43:52 +0300

    Finished mobile, tablets version

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
         nextArrow: "#slider .arrow-next"
     });
 
+    $('.js-reviews-slider').slick({
+        arrows: false,
+        dots: true
+    });
+
     if ($('.fancy').length > 0) {
         $('.fancy').fancybox();
         $('a.fancy').on("click", function (e) {
@@ -16,14 +21,20 @@ $(document).ready(function () {
 
     $('.js-hamburger').on('click', function(){
         $(this).toggleClass('is-active');
+        $('.nav-mobile').toggleClass('open');
+        $('#body').toggleClass('open');
     });
     
 
     $('input[name=jscode]').val('1');
     bannerLoadImg();
+    initSlick();
 });
 
-
+$(window).resize(function () {
+    bannerLoadImg();
+    initSlick();
+});
 
 function bannerLoadImg() {
     var sWidth = $(window).width();
@@ -36,7 +47,7 @@ function bannerLoadImg() {
         if (sWidth > 0) {
             dataProp = 'mobile';
         }
-        if (sWidth >= 767 && el.data('tablet') != undefined) {
+        if (sWidth >= 600 && el.data('tablet') != undefined) {
             dataProp = 'tablet';
         }
         if (sWidth >= 875 && el.data('desktop') != undefined) {
@@ -47,6 +58,21 @@ function bannerLoadImg() {
         }
     });
 }
-$(window).resize(function () {
-    bannerLoadImg();
-});
+function initSlick() {
+    var sWidth = $(window).width();
+    if (sWidth > 500) {
+        $('.js-slider-01.slick-initialized').slick("unslick");
+    }
+    else {
+        $('.js-slider-01:not(.slick-initialized)').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            variableWidth: false,
+            adaptiveHeight: true
+        });
+    }
+}
